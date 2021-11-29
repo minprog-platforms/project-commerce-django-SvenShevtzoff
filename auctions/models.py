5cc806f790d41559de200070dade919db955a10d
SvenShevtzoff <sshevtzoff@gmail.com>
2021-11-29 16:06:49 +0100

    Adding models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=1024)
+    starting_bid = models.DecimalField(decimal_places=2, max_digits=64)
+    image_link = models.CharField(max_length=1024)
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    comment = models.CharField(max_length=1024)
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
