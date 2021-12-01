b5ff52f9f8143fc34346435e07a391798deb0c76
SvenShevtzoff <sshevtzoff@gmail.com>
2021-12-01 16:19:44 +0100

    Implementing listing page and bidding functionality

--- auctions/models.py
+++ auctions/models.py
@@ -7,10 +7,13 @@ class User(AbstractUser):
 
 class Listing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    name = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
     description = models.CharField(max_length=1024)
     starting_bid = models.DecimalField(decimal_places=2, max_digits=64)
     image_link = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return f"{self.title}, {self.description}, {self.starting_bid}, {self.image_link}"
 
 class Comment(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
@@ -18,6 +21,6 @@ class Comment(models.Model):
     comment = models.CharField(max_length=1024)
 
 class Bid(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.IntegerField()
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,47 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, Listing, Bid
 
+class NewListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['title', 'description', 'starting_bid', 'image_link']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}), 
+            'description': forms.Textarea(attrs={'class': 'form-control'}), 
+            'starting_bid': forms.TextInput(attrs={'class': 'form-control'}), 
+            'image_link': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+class NewBidForm(forms.ModelForm):
+    amount = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter amount'}), label="")
+
+    class Meta:
+        model = Bid
+        fields = ['amount']
+
+        widgets = {
+            'amount': forms.TextInput(attrs={'class': 'form-control'})
+        }
+    
 
 def index(request):
-    return render(request, "auctions/index.html")
+    if request.method == 'POST':
+        form = NewListingForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.save()
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.all()
+    })
 
 
 def login_view(request):
@@ -61,3 +94,25 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required
+def create(request):
+    form = NewListingForm()
+    return render(request, "auctions/create.html", {
+        "form": form
+    })
+
+def listing(request, requested_title):
+    if request.method == 'POST':
+        bid_form = NewBidForm(request.POST)
+        if bid_form.is_valid():
+            bid_form = bid_form.save(commit=False)
+            bid_form.user = request.user
+            bid_form.listing = Listing.objects.filter(title=requested_title).first()
+            bid_form.save()
+    bid_form = NewBidForm()
+    listing = Listing.objects.filter(title=requested_title)
+    bids = listing.first().bids.all()
+    return render(request, "auctions/listing.html", {
+        "listing": listing.first(), "bids": bids, "bid_form": bid_form
+    })
